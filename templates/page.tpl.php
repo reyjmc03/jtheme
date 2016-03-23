96f5356f50200f3bb0b67c8c07a22fef49618704
reyjmc03 <reyjmc03@gmail.com>
2016-03-24 02:03:13 +0800

    03242016 0200 - adding image slider in templates/page.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -28,7 +28,29 @@
               <!-- Start profile_date -->
               <div class="profile_date">
                 <div class="profile_img">
-                  <img src="<?php print base_path() . drupal_get_path('theme', 'jtheme') . '/img/picc.jpg'; ?>" alt="" class="hz_image">
+                  <!-- <img src="<?php //print base_path() . drupal_get_path('theme', 'jtheme') . '/img/picc.jpg'; ?>" alt="" class="hz_image"> -->
+
+                  <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+                    <div class="carousel-inner" role="listbox">
+                      <div class="item">
+                        <img class="first-slide" src="<?php print base_path() . drupal_get_path('theme', 'jtheme') . '/img/picc-1.jpg'; ?>" alt="First slide">
+                      </div>
+                      <div class="item active">
+                        <img class="second-slide" src="<?php print base_path() . drupal_get_path('theme', 'jtheme') . '/img/picc-2.jpg'; ?>" alt="Second slide">
+                      </div>
+                      <div class="item">
+                        <img class="third-slide" src="<?php print base_path() . drupal_get_path('theme', 'jtheme') . '/img/picc-3.jpg'; ?>" alt="Third slide">
+                      </div>
+                    </div>
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
                 </div>
 
                 <div class="name_and_job">
